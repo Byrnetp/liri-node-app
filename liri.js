3376245941f473c440f7d2f4a455dd56122bfa09
Travis Byrne <travis.p.byrne@gmail.com>
2019-04-01 20:16:24 -0600

    added movie-this and do-what-it-says commands, spread output across multiple lines, removed spaces from the song and movie title input

--- liri.js
+++ liri.js
@@ -1,16 +1,36 @@
 'use strict';
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 const keys = require('./keys.js');
 const request = require('request');
 const Spotify = require('node-spotify-api');
 const moment = require('moment');
 const fs = require('fs');
 
-const command = process.argv[2];
-const option = process.argv[3];
+let command = process.argv[2];
+let option = process.argv[3];
+
+// do-what-it-says command
+if (command == 'do-what-it-says') {
+    let fileCommand = fs.readFileSync('./random.txt', 'utf8').split(' ');
+    command = fileCommand[0];
+    option = '';
+    for (let i = 1; i < fileCommand.length; i++) {
+        option = option.concat(fileCommand[i] + ' ');
+    }
+}
+
+// taking the spaces out of the song/movie/artist names as they will be used inside URLs
+if (option) {
+    for (let i = 0; i < option.length; i++) {
+        if (option[i] === ' ') {
+            option = option.slice(0,i) + '%20' + option.slice(i + 1, option.length);
+        }
+    }
+}
 
 const spotify = new Spotify(keys.spotify);
 
+// switch which contains commands for LIRI as cases
 switch (command) {
     case 'spotify-this-song':
         spotify
@@ -21,12 +41,49 @@ switch (command) {
                 for (let i = 1; i < artists.length; i++) {
                     artistList += `, ${artists[i].name}`;
                 }
-                console.log(`Song: ${response.tracks.items[0].name} Artist(s): ${artistList} Album: ${response.tracks.items[0].album.name} Preview Link: ${response.tracks.items[0].preview_url}`);
+                console.log(`Song: ${response.tracks.items[0].name} \n`
+                    + `Artist(s): ${artistList} \n`
+                    + `Album: ${response.tracks.items[0].album.name} \n`
+                    + `Preview Link: ${response.tracks.items[0].preview_url}`);
             })
             .catch((err) => {
                 console.log(err);
             });
         break;
+
+    case 'movie-this':
+        const OMDbUrl = `http://www.omdbapi.com/?apikey=${process.env.OMDb_key}&t=${option || 'Mr.%20Nobody'}`;
+        const promise = new Promise((resolve, reject) => {
+            request.get(OMDbUrl, (err, res, body) => {
+                if (err) {
+                    reject(`Error: ${err}`);
+                    return;
+                }
+                if (res.statusCode !== 200) {
+                    reject(`Error: ${JSON.parse(body)}`);
+                    return;
+                }
+                resolve(JSON.parse(body));
+            });
+        });
+
+        promise.then((body) => {
+            console.log(`Title: ${body.Title} \n`
+                + `Released: ${body.Year}\n`
+                + `IMDB Rating: ${body.imdbRating}\n`
+                + `Rotten Tomatoes rating: ${body.Ratings[1].Value}\n`
+                + `Produced in: ${body.Country}\n`
+                + `Language: ${body.Language}\n`
+                + `Actors/Actresses: ${body.Actors}\n`
+                + `Plot: ${body.Plot}`)
+        });
+        promise.catch((err) => console.log(err));
+
+        break;
+
     default:
-        console.log('LIRI commands: \nconcert-this "<song name>" -- returns song name, artist(s), album, and a preview link from Spotify.')
+        console.log('LIRI commands: \n'
+            + 'spotify-this-song "<song name>" -- returns song name, artist(s), album, and a preview link from Spotify. \n'
+            + 'movie-this "<Movie name>" -- returns title, release year, IMDB rating, Rotten Tomatoes rating, country of origin, language, plot, and actors in the movie from OMDb.\n'
+            + 'do-what-it-says -- Reads the command written in random.txt and runs said command.')
 }
